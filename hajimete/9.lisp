0d652ea0ef7b498dc1c17512e35840ed5f002993
Takeo Kondo <takeoverjp@gmail.com>
2023-01-24 00:03:24 +0900

    第９講　デートの前にリストの切り貼り(WIP)

--- hajimete/9.lisp
+++ hajimete/9.lisp
@@ -1,6 +1,12 @@
-(defun remove-1 (x y)
-(cond ((null y) nil)
-      ((eq x (car y)) (cdr y))
-      (t (cons (car y) (remove-1 x (cdr y))))))
+(defun my-remove-1 (x y)
+  (cond ((null y) nil)
+        ((eq x (car y)) (cdr y))
+        (t (cons (car y) (my-remove-1 x (cdr y))))))
 
-(print (remove-1 2 '(1 1 2 3 3)))
+(defun my-remove (x y)
+  (cond ((null y) nil)
+        ((eq x (car y)) (my-remove x (cdr y)))
+        (t (cons (car y) (my-remove x (cdr y))))))
+
+(print (my-remove-1 2 '(1 1 2 2 3 3)))
+(print (my-remove 2 '(1 1 2 2 3 3)))
