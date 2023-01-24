a8d4bf674afc1485a89b39cb2c4c08c24b6e4184
Takeo Kondo <takeoverjp@gmail.com>
2023-01-24 21:26:54 +0900

    第９講　デートの前にリストの切り貼り

--- hajimete/9.lisp
+++ hajimete/9.lisp
@@ -10,3 +10,11 @@
 
 (print (my-remove-1 2 '(1 1 2 2 3 3)))
 (print (my-remove 2 '(1 1 2 2 3 3)))
+
+(defun my-nreverse (x) (nrev2 x nil))
+(defun nrev2 (x r)
+  (cond ((null x) r)
+        (t (prog1 (nrev2 (cdr x) x)
+                  (rplacd x r)))))
+
+(print (my-nreverse '(1 2 3 4 5)))
