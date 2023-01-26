e30c58119bc21217a9afece26b198ba78bf9fb90
Takeo Kondo <takeoverjp@gmail.com>
2023-01-26 23:55:51 +0900

    第10講　WIP

--- hajimete/10.lisp
+++ hajimete/10.lisp
@@ -4,4 +4,15 @@
     (t (cons (my-subst new old (car tree))
              (my-subst new old (cdr tree))))))
 
+(defun my-subst2 (new old tree)
+  (cond ((eq old tree) new)
+        ((atom tree) tree)
+        (t (let ((a (subst new old (car tree)))
+                 (d (subst new old (cdr tree))))
+             (cond ((and (eq a (car tree))
+                         (eq d (cdr tree)))
+                   tree)
+                   (t (cons a d)))))))
+
 (print (my-subst 'kk nil '(a b (b a) nil a)))
+(print (my-subst2 'kk nil '(a b (b a) nil a)))
