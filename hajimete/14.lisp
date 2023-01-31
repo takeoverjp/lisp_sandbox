db890d45bb18947b2869eff2b12a3f5c0449ee64
Takeo Kondo <takeoverjp@gmail.com>
2023-02-01 02:16:27 +0900

    第１４講　入出力

--- hajimete/14.lisp
+++ hajimete/14.lisp
@@ -1,1 +1,9 @@
 (write (+ (read) (read)))
+
+
+(defun foo (x &optional (y 2) (z (+ x y)) &rest r) (append (list x y z) r))
+
+(print (foo 1 2 3 4 5 6))
+(print (foo 1 2 3))
+(print (foo 1 2))
+(print (foo 1))
