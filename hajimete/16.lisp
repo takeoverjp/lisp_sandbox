714210323edf0fe27a92dccb6c0b4cecb34f7f88
Takeo Kondo <takeoverjp@gmail.com>
2023-02-04 23:26:09 +0900

    第１６講　大団円

--- hajimete/16.lisp
+++ hajimete/16.lisp
@@ -1,1 +1,8 @@
-(loop (write (eval (read))))
+(defun tarai (x y z)
+  (cond ((<= x y) y)
+        (t (tarai (tarai (1- x) y z)
+                  (tarai (1- y) z x)
+                  (tarai (1- z) x y)))))
+(format t "tarai: ~A~%" (tarai 100000000000 110000000000 120000000000))
+
+(loop (princ  "> ") (write (eval (read))) (terpri))
---
+++ hajimete/error-factorial.lisp
@@ -0,0 +1,5 @@
+(defun factorial (n)
+  (cond ((= n 0) l)
+        (t (* n (factorial (1- n))))))
+
+(factorial 5)
