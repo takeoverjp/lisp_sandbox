0d9f8a3ccb7eb0b09d7611226ae98ec2f0d77ff7
Takeo Kondo <takeoverjp@gmail.com>
2023-03-10 22:06:05 +0900

    L-99: P17 Split a list into two parts; the length of the first part is given.

--- l-99/p17.lisp
+++ l-99/p17.lisp
@@ -1,5 +1,12 @@
 (defun split-list (l n)
-  (if (> n 0) (append (list (car l)) (split-list (cdr l) (1- n)))
-      (list l)))
+  (list (split-up-to l n) (split-after l n)))
+
+(defun split-up-to (l n)
+  (if (zerop n) nil
+      (cons (car l) (split-up-to (cdr l) (1- n)))))
+
+(defun split-after (l n)
+  (if (zerop n) l
+      (split-after (cdr l) (1- n))))
 
 (print (split-list '(a b c d e f g h i j k) 3))
