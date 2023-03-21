a5cdb5d06208587b95ebe1d45cf3d8669cb10cec
Takeo Kondo <takeoverjp@gmail.com>
2023-03-21 21:17:02 +0900

    L-99: P31 Determine whether a given integer number is prime.

---
+++ l-99/p31.lisp
@@ -0,0 +1,9 @@
+(defun is-prime (n)
+  (and (> n 1) (not (can-divide n (floor n 2)))))
+
+(defun can-divide (n m)
+  (if (<= m 1) nil
+      (if (= 0 (mod n m)) t (can-divide n (1- m)))))
+
+(print (is-prime 17))
+(print (is-prime 18))
