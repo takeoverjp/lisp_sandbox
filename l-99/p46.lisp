596a71b2f836516abd094c93194f30b8eaf00d8c
Takeo Kondo <takeoverjp@gmail.com>
2023-04-05 23:46:01 +0900

    L-99:P46 Truth tables for logical expressions.

--- l-99/p46.lisp
+++ l-99/p46.lisp
@@ -1,6 +1,8 @@
 (defun table (a b e)
   (dolist (va '(t nil))
     (dolist (vb '(t nil))
-      (format t "~S ~S ~%" va vb))))
+      (format t "~S ~S ~S~%"
+        va vb
+        (eval (subst vb b (subst va a e)))))))
 
 (table 'A 'B '(and A (or A B)))
