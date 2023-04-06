05d7f3aa89b052e55429be33b6718179033a6324
Takeo Kondo <takeoverjp@gmail.com>
2023-04-06 23:40:47 +0900

    L-99:P47 Truth tables for logical expressions (2).

---
+++ l-99/p47.lisp
@@ -0,0 +1,22 @@
+(defun table (a b e)
+  (dolist (va '(t nil))
+    (dolist (vb '(t nil))
+      (format t "~S ~S ~S~%"
+        va vb
+        (eval-infix a va b vb e)))))
+
+(defun eval-infix (a va b vb e)
+  (cond
+    ((eq e a) va)
+    ((eq e b) vb)
+    ((eq (car e) 'not) (not (eval-infix a va b vb (cadr e))))
+    ((eq (cadr e) 'and) (and (eval-infix a va b vb (car e)) (eval-infix a va b vb (caddr e))))
+    ((eq (cadr e) 'or) (or (eval-infix a va b vb (car e)) (eval-infix a va b vb (caddr e))))))
+
+(table 'A 'B 'A)
+(table 'A 'B 'B)
+(table 'A 'B '(not B))
+(table 'A 'B '(A and B))
+(table 'A 'B '(A or B))
+
+(table 'A 'B '(A and (A or (not B))))
