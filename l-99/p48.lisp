0c91d816d8f92140df04c2470ef81ea4bd3fccc4
Takeo Kondo <takeoverjp@gmail.com>
2023-04-13 23:55:16 +0900

    L-99:P48 WIP

--- l-99/p48.lisp
+++ l-99/p48.lisp
@@ -3,9 +3,14 @@
   (cond
     ((eq e nil) nil)
     ((eq e t) t)
-    ((eq (first e) 'not) (not (eval-infix (second e))))))
+    ((eq (first e) 'not) (not (eval-infix (second e))))
+    ((eq (second e) 'and) (and (eval-infix (first e)) (eval-infix (third e))))))
 
 (print (eval-infix t))
 (print (eval-infix nil))
 (print (eval-infix '(not t)))
 (print (eval-infix '(not nil)))
+(print (eval-infix '(t and t)))
+(print (eval-infix '(t and nil)))
+(print (eval-infix '(nil and t)))
+(print (eval-infix '(nil and nil)))
