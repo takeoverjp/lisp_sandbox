97193a59b642d384567ad234671f7f77d8df1965
Takeo Kondo <takeoverjp@gmail.com>
2023-04-19 23:30:58 +0900

    L-99:P49 WIP

--- l-99/p49.lisp
+++ l-99/p49.lisp
@@ -1,3 +1,7 @@
+(defun list-to-string (l)
+  (if (null l) ""
+    (format nil "~c~A" (car l) (list-to-string (cdr l)))))
+
 (defun gray-list (n)
   (if (= n 0)
       '(())
@@ -12,3 +16,6 @@
 (assert (equal (gray-list 1) '((#\0) (#\1))))
 (assert (equal (gray-list 2) '((#\0 #\0) (#\0 #\1) (#\1 #\1) (#\1 #\0))))
 (assert (equal (gray-list 3) '((#\0 #\0 #\0) (#\0 #\0 #\1) (#\0 #\1 #\1) (#\0 #\1 #\0) (#\1 #\1 #\0) (#\1 #\1 #\1) (#\1 #\0 #\1) (#\1 #\0 #\0))))
+
+(assert (equal "001" (list-to-string '(#\0 #\0 #\1))))
+(assert (equal "111" (list-to-string '(#\1 #\1 #\1))))
