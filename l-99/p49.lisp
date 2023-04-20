916018bb8ed33b3e93a19800aca12aa83c0be960
Takeo Kondo <takeoverjp@gmail.com>
2023-04-20 23:11:26 +0900

    L-99:P49 Gray code.

--- l-99/p49.lisp
+++ l-99/p49.lisp
@@ -1,3 +1,6 @@
+(defun gray-code (n)
+  (mapcar #'list-to-string (gray-list n)))
+
 (defun list-to-string (l)
   (if (null l) ""
     (format nil "~c~A" (car l) (list-to-string (cdr l)))))
@@ -19,3 +22,7 @@
 
 (assert (equal "001" (list-to-string '(#\0 #\0 #\1))))
 (assert (equal "111" (list-to-string '(#\1 #\1 #\1))))
+
+(assert (equal (gray-code 1) '("0" "1")))
+(assert (equal (gray-code 2) '("00" "01" "11" "10")))
+(assert (equal (gray-code 3) '("000" "001" "011" "010" "110" "111" "101" "100")))
