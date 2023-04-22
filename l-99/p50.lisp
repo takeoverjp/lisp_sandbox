6ae43bace190ca7a2f6200d936028cc46e269db3
Takeo Kondo <takeoverjp@gmail.com>
2023-04-22 23:47:51 +0900

    L-99:P50 WIP

--- l-99/p50.lisp
+++ l-99/p50.lisp
@@ -1,2 +1,7 @@
 (defun huffman (l) ())
 (print (huffman '((a 45) (b 13) (c 12) (d 16) (e 9) (f 5))))
+
+(defun huf-encode (l)
+  (list (first l) (second l) nil nil))
+
+(assert (equal '(a 45 nil nil) (huf-encode '(a 45))))
