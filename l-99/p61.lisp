02e0cebc80e9fad746c9fff2a62a4b49011cc0f5
Takeo Kondo <takeoverjp@gmail.com>
2023-05-10 20:33:34 +0900

    L-99:p61 P61 Count the leaves of a binary tree

---
+++ l-99/p61.lisp
@@ -0,0 +1,11 @@
+(defun count-leaves (tree)
+  (cond ((null tree) 0)
+        ((atom tree) 0)
+        ((and (null (second tree))
+              (null (third tree))) 1)
+        (t (+ (count-leaves (second tree)) (count-leaves (third tree))))))
+
+(assert (= 0 (count-leaves nil)))
+(assert (= 1 (count-leaves '(x nil nil))))
+(assert (= 2 (count-leaves '(x (x nil nil) (x nil nil)))))
+(assert (= 2 (count-leaves '(x (x nil (x nil nil)) (x nil nil)))))
