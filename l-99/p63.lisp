23e3075269bf4f866e39a1644c01739b8d1fc1c3
Takeo Kondo <takeoverjp@gmail.com>
2023-05-15 22:41:17 +0900

    L-99:P63  Construct a complete binary tree

--- l-99/p63.lisp
+++ l-99/p63.lisp
@@ -13,3 +13,23 @@
 (assert (= 4 (highest-2-power 6)))
 (assert (= 4 (highest-2-power 7)))
 (assert (= 8 (highest-2-power 8)))
+
+(defun complete-binary-tree (n)
+  (cbt-label n 1))
+
+(defun cbt-label (n k)
+  (cond
+    ((= n 0) nil)
+    ((= n 1) (list k nil nil))
+    (t (let* ((p (floor (highest-2-power n)2))
+              (left (if (= 2 (floor n p))
+                        (+ p (mod n p))
+                      (+ p (1- p)))))
+         (list k
+               (cbt-label left (* 2 k))
+               (cbt-label (- n 1 left) (+ 1 (* 2 k))))))))
+
+(assert (equal '(1 nil nil) (complete-binary-tree 1)))
+(assert (equal '(1 (2 nil nil) nil) (complete-binary-tree 2)))
+(assert (equal '(1 (2 nil nil) (3 nil nil)) (complete-binary-tree 3)))
+(assert (equal '(1 (2 (4 nil nil) nil) (3 nil nil)) (complete-binary-tree 4)))
