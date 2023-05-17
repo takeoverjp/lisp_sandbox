7d23ec819b10f2e2aa7d5d02b064155074c251a2
Takeo Kondo <takeoverjp@gmail.com>
2023-05-17 22:50:32 +0900

    L-99:P64: WIP

--- l-99/p64.lisp
+++ l-99/p64.lisp
@@ -6,3 +6,14 @@
 (assert (= 0 (size nil)))
 (assert (= 1 (size '(x nil nil))))
 (assert (= 3 (size '(x (l nil nil) (r nil nil)))))
+
+(defun move (x y tree)
+  (if (null tree) nil
+    (list
+      (first tree)
+      (+ x (second tree))
+      (+ y (third tree))
+      (move x y (fourth tree))
+      (move x y (fifth tree)))))
+
+(assert (equal '(w 2 4 nil nil) (move 1 2 '(w 1 2 nil nil))))
