b1df65e803b9bce84dbd0126561da681078497c0
Takeo Kondo <takeoverjp@gmail.com>
2023-05-18 21:20:41 +0900

    L-99:P64: Layout a binary tree

--- l-99/p64.lisp
+++ l-99/p64.lisp
@@ -17,3 +17,18 @@
       (move x y (fifth tree)))))
 
 (assert (equal '(w 2 4 nil nil) (move 1 2 '(w 1 2 nil nil))))
+
+(defun layout-binary-tree (tree)
+  (if (null tree)
+    nil
+    (let ((lsize (size (second tree))))
+      (list
+        (first tree)
+        (1+ lsize)
+        1
+        (move 0 1 (layout-binary-tree (second tree)))
+        (move 0 1 (move (1+ lsize) 0 (layout-binary-tree (third tree))))))))
+
+(assert (equal nil (layout-binary-tree '())))
+(assert (equal '(x 1 1 nil nil) (layout-binary-tree '(x nil nil))))
+(assert (equal '(x 2 1 (l 1 2 nil nil) (r 3 2 nil nil)) (layout-binary-tree '(x (l nil nil) (r nil nil)))))
